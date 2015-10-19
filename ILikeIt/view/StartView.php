9a3a6a22ba963c6cbea82ac8a19852c78bdd947a
Lisa Westlund <lw222gu@student.lnu.se>
2015-10-19 16:21:27 +0200

    Added css and changed information in UI

--- ILikeIt/view/StartView.php
+++ ILikeIt/view/StartView.php
@@ -17,11 +17,14 @@ class StartView {
 
     public function generateRegisterButton(){
         return '
+            <p>Välkommen till I like it! Här skapar du en personlig startsida med de länkar du vill ha nära till hands.
+            Fyll i ditt namn nedan och klicka på den stora knappen så är du igång. Kom ihåg att spara adressen du länkas
+            vidare till - det är nämligen så du kommer åt dina länkar senare. Spar den som startsida i webbläsaren, eller
+            som ett bokmärke.</p>
             <form method="post" >
-                <fieldset>
-                    <input type="input" name ="' . self::$name . '" value="" />
-                    <input type="submit" name="' . self::$register . '" value="Skapa en egen startsida!" />
-                </fieldset>
+                <label for="' . self::$name . '" id="nameLabel">Ditt namn</label>
+                <input type="input" name ="' . self::$name . '" id="nameInput" value="" />
+                <input type="submit" name="' . self::$register . '" id="registerButton" value="Skapa din startsida!" />
             </form>
         ';
     }
